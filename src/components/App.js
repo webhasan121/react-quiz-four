a3ecc616019a36d3767bcf853aaadc4b9fce3401
hasanur rahman <sm.hasanur121.com>
2021-10-18 12:24:55 +0600

    lesson -4 sourch code push

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <Layout>
+      <Home></Home>
+    </Layout>
+  );
+}
+
+export default App;
